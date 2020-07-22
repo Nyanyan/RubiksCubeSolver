b78e690f1ac727dd13123762bd3a56d61753b23e
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-07-22 21:54:50 +0900

    first commit

---
+++ cube_class.py
@@ -0,0 +1,98 @@
+'''
+Corner
+   B
+  0 1 
+L 2 3 R
+   F
+
+   F
+  4 5
+L 6 7 R
+   B
+
+
+Edge
+top layer
+    B
+    0
+L 3   1 R
+    2
+    F
+
+middle layer
+4 F 5 R 6 B 7
+
+bottom layer
+    F
+    8
+L 11  9 R
+    10
+    B
+'''
+
+'''
+Solver:
+phase 0: gather UD stickers on UD faces and clear EO
+         use R, L, U, D, F, B, not use R2, L2, F2, B2
+phase 1: solve it!
+         use R2, L2, U, D, F2, B2
+'''
+
+
+from time import time
+
+move_candidate = ["R", "R2", "R'", "L", "L2", "L'", "U", "U2", "U'", "D", "D2", "D'", "F", "F2", "F'", "B", "B2", "B'"]
+
+class Cube:
+    def __init__(self, cp=list(range(8)), co=[0 for _ in range(8)], ep=list(range(12)), eo=[0 for i in range(12)]):
+        self.Cp = cp
+        self.Co = co
+        self.Ep = ep
+        self.Eo = eo
+    
+    def move_cp(self, mov):
+        surface = [[3, 1, 7, 5], [0, 2, 4, 6], [0, 1, 3, 2], [4, 5, 7, 6], [2, 3, 5, 4], [1, 0, 6, 7]]
+        res = [i for i in self.Cp]
+        mov_type = mov // 3
+        mov_amount = mov % 3
+        for i in range(4):
+            res[surface[mov_type][(i + mov_amount + 1) % 4]] = self.Cp[surface[mov_type][i]]
+        return res
+    
+    def move_co(self, mov):
+        surface = [[3, 1, 7, 5], [0, 2, 4, 6], [0, 1, 3, 2], [4, 5, 7, 6], [2, 3, 5, 4], [1, 0, 6, 7]]
+        pls = [2, 1, 2, 1]
+        res = [i for i in self.Co]
+        mov_type = mov // 3
+        mov_amount = mov % 3
+        for i in range(4):
+            res[surface[mov_type][(i + mov_amount + 1) % 4]] = self.Co[surface[mov_type][i]]
+            if mov_type // 2 != 1 and mov_amount != 1:
+                res[surface[mov_type][(i + mov_amount + 1) % 4]] += pls[(i + mov_amount + 1) % 4]
+                res[surface[mov_type][(i + mov_amount + 1) % 4]] %= 3
+        return res
+    
+    def move_ep(self, mov):
+        surface = [[1, 6, 9, 5], [3, 4, 11, 7], [0, 1, 2, 3], [8, 9, 10, 11], [2, 5, 8, 4], [0, 7, 10, 6]]
+        res = [i for i in self.Ep]
+        mov_type = mov // 3
+        mov_amount = mov % 3
+        for i in range(4):
+            res[surface[mov_type][(i + mov_amount + 1) % 4]] = self.Ep[surface[mov_type][i]]
+        return res
+    
+    def move_eo(self, mov):
+        surface = [[1, 6, 9, 5], [3, 4, 11, 7], [0, 1, 2, 3], [8, 9, 10, 11], [2, 5, 8, 4], [0, 7, 10, 6]]
+        res = [i for i in self.Ep]
+        mov_type = mov // 3
+        mov_amount = mov % 3
+        for i in range(4):
+            res[surface[mov_type][(i + mov_amount + 1) % 4]] = self.Eo[surface[mov_type][i]]
+        if mov // 6 == 2:
+            for i in range(4):
+                res[i] += 1
+                res[i] %= 2
+        return res
+    
+    def move(self, mov):
+        return Cube(cp=self.move_cp(mov), co=self.move_co(mov), ep=self.move_ep(mov), eo=self.move_eo(mov))
